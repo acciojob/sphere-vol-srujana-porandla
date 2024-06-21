b33dbaf840875c5471c88fd68e78925402bf3c9b
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-21 13:03:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ function volume_sphere(event) {
 	var r=document.getElementById("radius").value;
 	var newr=parseInt(r);
 	if(newr<0){
-		document.getElementById("volume").textContent="NaN";
+		document.getElementById("volume").value="NaN";
 		
 	}
 	else{
 		var v=(4/3) * Math.PI * Math.pow(newr, 3);
 		//var v= \frac{4}{3} \pi r^3;
 		v=v.toFixed(4);
-		document.getElementById("volume").textContent = v;
+		document.getElementById("volume").value= v;
 	}
   
 } 
